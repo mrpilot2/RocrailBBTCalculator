9c93772a4f395a5169100ba4c72e1c1976bd2c5b
Markus Reitboeck <mrpilot@gmx.net>
2018-10-07 10:47:52 +0200

    (-) fix filter for "mainline only"
    when switching to a custom sort/filter model, the text beside
    the icon was replaced with a tooltip. The original sort/filter model
    used this text to filter for mainline. Now the tooltip is used

--- src/gui/imagesortfilterproxymodel.cpp
+++ src/gui/imagesortfilterproxymodel.cpp
@@ -6,12 +6,26 @@
 
 #include "gui/bbtmodel.hpp"
 
-using BBTCalculator::Gui::ImageSortFilterProxyModel;
 using BBTCalculator::Gui::BBTModel;
+using BBTCalculator::Gui::ImageSortFilterProxyModel;
 
 ImageSortFilterProxyModel::ImageSortFilterProxyModel(QObject* parent)
     : QSortFilterProxyModel(parent)
 {
+}
+
+bool ImageSortFilterProxyModel::filterAcceptsRow(
+    int source_row, const QModelIndex& source_parent) const
+{
+    for (int i = 0; i < sourceModel()->columnCount(source_parent); ++i) {
+        QModelIndex index = sourceModel()->index(source_row, i, source_parent);
+        if (index.data( Qt::DecorationRole).type() == QVariant::Icon) {
+            return index.data(Qt::ToolTipRole).toString().contains(
+                filterRegExp());
+        }
+    }
+
+    return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
 }
 
 bool ImageSortFilterProxyModel::lessThan(const QModelIndex& source_left,
@@ -22,9 +36,8 @@ bool ImageSortFilterProxyModel::lessThan(const QModelIndex& source_left,
         return source_left.data(Qt::ToolTipRole) >
                source_right.data(Qt::ToolTipRole);
     }
-    else {
+    else
+    {
         return QSortFilterProxyModel::lessThan(source_left, source_right);
     }
-
-
 }
--- src/include/gui/imagesortfilterproxymodel.hpp
+++ src/include/gui/imagesortfilterproxymodel.hpp
@@ -13,6 +13,8 @@ namespace BBTCalculator
            explicit ImageSortFilterProxyModel(QObject* parent = nullptr);
 
         protected:
+            bool filterAcceptsRow(int source_row,
+                                  const QModelIndex& source_parent) const override;
 
             bool lessThan(const QModelIndex& source_left, const QModelIndex& source_right) const override;
         };
